d93baa5b534049f50fa3375ef506a5cae0829c27
Jens Schneider <jens.schneider.ac@posteo.de>
2022-05-31 15:31:36 +0200

    Correct comment character in license
    Signed-off-by: Jens Schneider <jens.schneider.ac@posteo.de>

--- pkg/cmd/webhook_options.go
+++ pkg/cmd/webhook_options.go
@@ -1,6 +1,6 @@
-# SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
-#
-# SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
 
 package cmd
 
